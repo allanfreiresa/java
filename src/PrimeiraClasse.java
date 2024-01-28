effd385bb8d28da65ef0692c246da83ef82d8553
Allan Freire <allanfreiresa@gmail.com>
2024-01-28 17:47:45 -0300

    segunda alteracao

--- src/PrimeiraClasse.java
+++ src/PrimeiraClasse.java
@@ -1,7 +1,7 @@
 public class PrimeiraClasse {
 
     public static void main(String[] args) {
-        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java");
     }
 
 }
